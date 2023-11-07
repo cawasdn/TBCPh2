942bd42c2f2e7d9fb972828b0614ddf8e640d8c8
Daniel <cawasdn@gmail.com>
2023-11-07 23:58:59 +0800

    first commit

---
+++ app/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='header-box'>
+        <div className='header-info-box'>
+          <h1>The Big Cup</h1>
+          <p>Where taste meet imagination.</p>
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header
---
+++ app/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <ul>
+        <a href='/home'>Home</a>
+        <a href='/about'>About</a>
+        <a href='/services'>Services</a>
+      </ul>
+    </div>
+  )
+}
+export default Navbar
