18c6c9bac54a56337eaab033347787f3d7bfa00e
Daniel <cawasdn@gmail.com>
2023-12-28 21:58:29 +0800

    fix

--- app/components/Testimonials.js
+++ app/components/Testimonials.js
@@ -1,12 +1,18 @@
 const Testimonials = () => {
   return (
     <section className='testimonial'>
-      <h1 className='testimonial-title'>We Rebuild Lives</h1>
+      <div className='bg-video'>
+        <video className='bg-video__content' autoPlay muted loop>
+          <source src='/testimonial-bg-2.mp4' type='video/mp4' />
+          Your Browser is not supported.
+        </video>
+      </div>
+      {/* <h1 className='testimonial-title'>We Rebuild Lives</h1>
       <div className='testimonial-video-box'>
         <video className='testimonial-video' autoPlay muted loop>
           <source src={'/testimonial-bg-2.mp4'} type='video/mp4'></source>
         </video>
-      </div>
+      </div> */}
     </section>
   )
 }
