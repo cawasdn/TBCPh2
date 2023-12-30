4f452fe986145d2169bcb0606e28fa5decab3f68
Daniel <cawasdn@gmail.com>
2023-12-31 01:21:39 +0800

    testimonial section fix

--- app/components/Testimonials.js
+++ app/components/Testimonials.js
@@ -2,14 +2,18 @@ const Testimonials = () => {
   return (
     <section className='testimonial'>
       <div className='u-center-text u-margin-bottom-big'>
-        <h2>OUR MENU</h2>
+        <h2>REVIEWS FROM OUR CUSTOMERS</h2>
       </div>
-      <div className='bg-video'>
+      <div className='row'>
+        <div className='story'>TEXt</div>
+      </div>
+
+      {/* <div className='bg-video'>
         <video className='bg-video__content' autoPlay muted loop>
           <source src='/testimonial-bg-2.mp4' type='video/mp4' />
           Your Browser is not supported.
         </video>
-      </div>
+      </div> */}
     </section>
   )
 }
