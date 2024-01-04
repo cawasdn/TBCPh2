d1ebec569333b9775c7f179b79a67b54c9bd85fa
Daniel <cawasdn@gmail.com>
2024-01-04 15:35:10 +0800

    Reviews added

--- app/components/Testimonials.js
+++ app/components/Testimonials.js
@@ -5,7 +5,22 @@ const Testimonials = () => {
         <h2>REVIEWS FROM OUR CUSTOMERS</h2>
       </div>
       <div className='row'>
-        <div className='story'>TEXt</div>
+        <div className='story'>
+          <figure className='story__shape'>
+            <img src='rev1.jpg' alt='image1' className='story__img' />
+          </figure>
+          <div className='story__text'>
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              Niknik Badidles
+            </h3>
+            <p>
+              Ang ganda ng ambiance. Nakaka relax, masarap tambayan with
+              friends. 24/7 din matitikman ang masasarap nilang food at mga
+              drinks. Large tea selection · Healthy salads · Delicious
+              sandwiches
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* <div className='bg-video'>
